5681c824d2ad586e21f5b3f3dedf3536ea4eb685
Miguel Angelo Vicente Neves <miguelangeloneves96@gmail.com>
2026-07-31 18:29:27 +0100

    menu 2

--- src/views/menu.py
+++ src/views/menu.py
@@ -6,4 +6,20 @@ while True:
     print("2. Listar técnicos")
     print("3. Gerir técnicos")
     print("4. Apagar técnicos")
+    print("0. Sair")
+
+    opcao = input("Escolha uma opção: ")
+
+    if opcao == "1":
+
+
+
+
+    if opcao == "2":
+
+
+
+    if opcao == "3":
+            
+          
         
