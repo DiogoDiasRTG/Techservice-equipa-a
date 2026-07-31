f52f7442d7d68e623e63d621adb6679c86f62872
Miguel Angelo Vicente Neves <miguelangeloneves96@gmail.com>
2026-07-31 18:43:41 +0100

    menu (opcao 1 e 2)

--- src/views/menu.py
+++ src/views/menu.py
@@ -15,16 +15,30 @@ while True:
 
     if opcao == "1":
 
+      nome = input("Digite o nome do técnico: ")
+      telefone = input("Digite o telefone do técnico: ")
+      email = input("Digite o email do técnico: ")
+      especialidade = input("Digite a especialidade do técnico: ")
 
+      tecnico = inserir(tecnico(nome=nome, telefone=telefone, email=email, especialidade=especialidade))
 
+      print(f"Técnico {tecnico.nome} inserido com sucesso!")
+      
 
     if opcao == "2":
 
-
+     tecnico=listar()
+     if tecnico:
+        
+        print("Lista de técnicos:")
+        for tecnico in tecnico:
+            print(f"ID: {tecnico.id_tecnico}, Nome: {tecnico.nome}, Telefone: {tecnico.telefone}, Email: {tecnico.email}, Especialidade: {tecnico.especialidade}")
+        else:
+            print("Nenhum técnico encontrado.")    
 
     if opcao == "3":
 
-
+     
 
     if opcao == "4":
 
